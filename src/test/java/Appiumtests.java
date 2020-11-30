ac640160be485bbc383a97798efa2da512418b27
Maianhanh0906 <43576900+Maianhanh0906@users.noreply.github.com>
2020-11-30 11:35:09 +0700

    openApp

--- src/test/java/Appiumtests.java
+++ src/test/java/Appiumtests.java
@@ -1,5 +1,6 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -19,8 +20,7 @@ public class Appiumtests {
 			System.out.println(exp.getCause());
 			System.out.println(exp.getMessage());
 			exp.printStackTrace();
-		}
-				
+		}				
 	}
 	
 	public static void openAppiumtests() throws Exception {
@@ -30,16 +30,16 @@ public class Appiumtests {
 		cap.setCapability("udid", "emulator-5554");
 		cap.setCapability("platformName", "Android");		
 		cap.setCapability("platformVersion", "11");
+		cap.setCapability("fullReset", "true");
+		cap.setCapability("noReset", "false");
 		cap.setCapability("appPackage", "com.blue.hoantran.itro_host");
-		cap.setCapability("appActivity", "com.blue.hoantran.itro_host.ui_v2.login.SelectPurposeActivity");
+		cap.setCapability("appActivity", "com.blue.hoantran.itro_host.ui_v2.login.SplashActivity");
 		
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AppiumDriver<MobileElement>(url , cap);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
-		System.out.println("Application Started");
-		
-		
-		
+		System.out.println("Application Started");				
 	}
 
 }
